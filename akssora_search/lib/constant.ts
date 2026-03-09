5dab39abb73aa5cc5565d239c8535d38e4bbb3ff
Akash-Kumar-Sinha <akashkumarsinha304@gmail.com>
2026-03-09 11:36:37 +0530

    Revert "implemented the upload and search query"
    This reverts commit 1d69ff45e9454b5f419bad5e7d5513b72937f045.

--- akssora_search/lib/constant.ts
+++
@@ -1,4 +0,0 @@
-export const EMAIL = "contact@akssora.com";
-
-export const BACKEND_URL = "http://localhost:8000/api/v1";
-export const AUTH_URL = `${BACKEND_URL}/auth`;
--- search_model_service/app/lib/file_name.py
+++
@@ -1,8 +0,0 @@
-import time
-
-def file_name(file):
-    timestamp = int(time.time())
-    safe_filename = file.filename.replace(" ", "_")
-    filename = f"{timestamp}_{safe_filename}"
-    file_location = f"/tmp/{filename}"
-    return file_location
